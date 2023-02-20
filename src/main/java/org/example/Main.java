8624002d4bea2b1e09e5c7b8ee4180f5ad4445e6
potatoCompletion <033kws2628@gmail.com>
2023-02-20 13:50:36 +0900

    로그 작업 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,33 @@
 package org.example;
 
 import com.opencsv.exceptions.CsvException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.logging.Logger;
 
 public class Main {
-    private final static Logger LOG = Logger.getGlobal();
-
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws IOException, CsvException {
+
+        log.info("==========학교 검색 시작==========");
         SchoolFinder schoolFinder = new SchoolFinder("comments.csv", "전국초중고대데이터.csv");
-        Map<String, Integer> result = schoolFinder.findValidSchool();
+        Map<String, Integer> schoolCountMap = schoolFinder.findValidSchool();
         FileWriter fileWriter = new FileWriter("result.txt");
 
-        for (String key : result.keySet()) {
-            fileWriter.write(key + "\t" + result.get(key) + "\n");
+        int findedSum = 0;
+        for (Map.Entry<String, Integer> entry : schoolCountMap.entrySet()) {
+            fileWriter.write(entry.getKey() + "\t" + entry.getValue() + "\n");
+            if (!entry.getKey().equals("알수없음")) {
+                findedSum += entry.getValue();
+            }
         }
 
         fileWriter.close();
-        LOG.info("로깅로깅");
+        log.info("발견된 학교 수: {}개", findedSum);
+        log.info("미발견 데이터: {}개", schoolCountMap.get("알수없음"));
+        log.info("==========학교 검색 종료==========");
     }
-
 }
--- src/main/java/org/example/SchoolFinder.java
+++ src/main/java/org/example/SchoolFinder.java
@@ -2,7 +2,8 @@ package org.example;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
-import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 public class SchoolFinder {
+    private static final Logger log = LoggerFactory.getLogger(SchoolFinder.class);
     private final List<String[]> originSchoolLines;
     private final List<String[]> sourceFileLines;
     private final List<String> originSchoolList;
@@ -25,25 +27,27 @@ public class SchoolFinder {
         originSchoolLines = schoolsCsvReader.readAll();
         sourceFileLines = sourceFileCsvReader.readAll();
 
-        originSchoolList = stringArrayListToList(originSchoolLines);
-        filteredSchoolList = getFilteredSchoolList(originSchoolList);
+        originSchoolList = stringArrayListToList(originSchoolLines);    // 전국 학교 정식명칭 리스트
+        filteredSchoolList = getFilteredSchoolList(originSchoolList);   // 전국 학교 약어 리스트 (ex: 강릉초등학교 -> 강릉초)
     }
 
-    public static class SchoolFinderBuilder {};
-
     public Map<String, Integer> findValidSchool() throws IOException {
-        Map<String, Integer> result = new HashMap<>();
+
+        Map<String, Integer> schoolCountMap = new HashMap<>();
 
         for (String[] line : sourceFileLines) {
+            // 1차 필터 (정식명칭)
             String filteredLine = String.join("", line)
                     .replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣]", ""); // 한글제외 모든 불필요 문자 제거
 
             String school = findAtSchoolList(filteredLine, originSchoolList);
             if (!school.isBlank()) {
-                result.merge(school, 1, Integer::sum);
+                schoolCountMap.merge(school, 1, Integer::sum);
+                log.info("{} 발견", school);
                 continue;
             }
 
+            // 2차 필터 (약어)
             filteredLine = filteredLine
                     .replace("등학교", "")  // OO초, OO중, OO고, OO대 라고 입력한 데이터 또한 유효한 문자열로 인식하기 위한 작업
                     .replace("중학교", "중")
@@ -53,40 +57,44 @@ public class SchoolFinder {
                     .replace("여중", "여자중")
                     .replace("체고", "체육고")
                     .replace("여고", "여자고")
+                    .replace("예중", "예술중")
                     .replace("예고", "예술고")
                     .replace("여대", "여자대");
 
             school = findAtSchoolList(filteredLine, filteredSchoolList);
             if (!school.isBlank()) {
-                result.merge(school, 1, Integer::sum);
+                schoolCountMap.merge(school, 1, Integer::sum);
+                log.info("{} 발견", school);
                 continue;
             }
 
-            result.merge("기타", 1, Integer::sum);
+            // 정식명칭, 약어 두 리스트에서 모두 찾을 수 없을 경우
+            schoolCountMap.merge("알수없음", 1, Integer::sum);
+            log.warn("(WARNING)알수없는 라인\n\n\"{}\"\n\n", filteredLine);
         }
 
-        return result;
+        return schoolCountMap;
     }
 
     private List<String> getFilteredSchoolList(List<String> originSchoolList) {
+
         List<String> filteredList = new ArrayList<>();
+
         for (String line : originSchoolList) {
-            String filteredString = line
+            filteredList.add(line
                     .replace(" ", "")
                     .replace("등학교", "")
                     .replace("중학교", "중")
                     .replace("대학교", "대")
                     .replace("대학", "대")
-                    .replace("사범대", "");
-
-
-            filteredList.add(filteredString);
+                    .replace("사범대", ""));
         }
 
         return filteredList;
     }
 
     private List<String> stringArrayListToList(List<String[]> stringArrayList) {
+
         List<String> list = new ArrayList<>();
         for (String[] stringArray : stringArrayList) {
             String str = String.join("", stringArray);
@@ -96,15 +104,16 @@ public class SchoolFinder {
         return list;
     }
 
-    private String findAtSchoolList(String comment, List<String> schoolList) {
-        for (String school : schoolList) {
+    private String findAtSchoolList(String line, List<String> schoolList) {
 
-            if (comment.contains(school)) {
+        for (String school : schoolList) {
+            if (line.contains(school)) {
                 int index = schoolList.indexOf(school);
-                return String.join("", originSchoolLines.get(index));
+                return originSchoolList.get(index); // 학교의 정식명칭으로 통일화해서 Count
             }
         }
 
         return "";
     }
+
 }
